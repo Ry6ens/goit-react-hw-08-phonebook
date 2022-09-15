ba338af38d6cefd2850479d81605d6c0fd79f14f
Sergey Gulaga <ry6ens@gmail.com>
2022-09-15 19:12:47 +0300

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,6 @@
-// import axios from "axios";
+import axios from "axios";
 
 import { instance } from "./apiAuth";
-
-// const instance = axios.create({
-//   baseURL: "https://631878cbf6b281877c6d54c6.mockapi.io",
-// });
 
 export const fetchPostContacts = async (contact) => {
   return await instance.post("/contacts", contact);
