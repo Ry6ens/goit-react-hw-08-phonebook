fd684273e0d32fa36d7920ddbdc40f92090914fe
Sergey Gulaga <ry6ens@gmail.com>
2022-09-15 22:02:49 +0300

    update styles

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -3,19 +3,26 @@ import ContactForm from "../../components/ContactForm/ContactForm";
 import ContactList from "../../components/ContactList/ContactList";
 import Filter from "../../components/Filter/Filter";
 import styles from "./Contacts.module.scss";
+import { Container } from "../../components/Container/Container";
 
 export default function Contacts() {
   return (
     <>
-      <div className={styles.contacts}>
-        <div>
-          <Section title="Phonebook">
-            <ContactForm />
-          </Section>
-        </div>
-        <div className={styles.list}>
-          <Filter />
-          <ContactList />
+      <div className="backdrop">
+        <div className="backdropGradient">
+          <Container>
+            <div className={styles.contacts}>
+              <div className={styles.contactForm}>
+                <Section title="Phonebook">
+                  <ContactForm />
+                </Section>
+              </div>
+              <div className={styles.list}>
+                <Filter />
+                <ContactList />
+              </div>
+            </div>
+          </Container>
         </div>
       </div>
     </>
